11dd32b98854e3586364f4d574b4b717aa9ae529
taokyone <antonsson.andreas@hotmail.com>
2023-09-04 17:54:34 +0200

    Added passed and failed ViewController

---
+++ navigation_app_practice/FailedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FailedViewController.swift
+//  navigation_app_practice
+//
+//  Created by Andreas Antonsson on 2023-09-04.
+//
+
+import UIKit
+
+class FailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ navigation_app_practice/PassedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PassedViewController.swift
+//  navigation_app_practice
+//
+//  Created by Andreas Antonsson on 2023-09-04.
+//
+
+import UIKit
+
+class PassedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
