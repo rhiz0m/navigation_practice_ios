640d607f5da5768f75c869c87207e695680522ed
taokyone <antonsson.andreas@hotmail.com>
2023-09-05 12:27:29 +0200

    Nil-check debugging. Making the tap image work

--- navigation_app_practice/ViewController.swift
+++ navigation_app_practice/ViewController.swift
@@ -7,17 +7,33 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
-
-
+    
+   // Lägg till denna IBOutlet om den inte redan finns
+ 
+    @IBOutlet weak var ellipse1: UIImageView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        /*
+        if let ellipse = ellipse1 {
+            // debugging. Kör ellipse som är garanterat inte nil
+            print("It works!")
+        } else {
+            // Om ellipse1 är nil
+            print("ellipse1 is nil")
+        }*/
+        
+        ellipse1.isUserInteractionEnabled = true
+        // Aktivera användargränssnittsinteraktioner för "Ellipse 1"
+    }
+    
+    @IBAction func tapEllipse(_ sender: UITapGestureRecognizer) {
+        print("It works!")
     }
     
 }
+
 
